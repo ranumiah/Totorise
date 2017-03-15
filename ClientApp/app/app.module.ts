1a20f925735014bd0585a271f31f9405430c7532
Ranu <O@O.net>
2017-03-15 11:51:30 +0000

    Add Weather Component

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { HelloWorldComponent } from './components/helloworld/helloworld.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 // So this guy describes the class that follows it
 @NgModule({
@@ -20,7 +21,8 @@ import { HelloWorldComponent } from './components/helloworld/helloworld.componen
         CounterComponent,
         FetchDataComponent,
         HomeComponent,
-        HelloWorldComponent
+        HelloWorldComponent,
+        WeatherComponent
     ],
     imports: [  // What stuff do I need?
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
@@ -30,6 +32,7 @@ import { HelloWorldComponent } from './components/helloworld/helloworld.componen
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: 'hello-world', component: HelloWorldComponent },
+            { path: 'weather', component: WeatherComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ],
---
+++ ClientApp/app/components/weather/weather.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'weather',
+    templateUrl: './weather.component.html'
+})
+
+export class WeatherComponent {
+    public weather: Weather;
+
+    constructor() {
+        this.weather = { temp: "12", summary: "Barmy", city: "London" };
+    }
+}
+
+interface Weather {
+    temp: string;
+    summary: string;
+    city: string;
+}
