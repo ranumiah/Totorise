829524135234c5c839610bba6f46e44ac636c5f0
Ranu <O@O.net>
2017-03-17 16:50:12 +0000

    Update CSS styles for gallery.component.ts

--- ClientApp/app/components/gallery/gallery.component.ts
+++ ClientApp/app/components/gallery/gallery.component.ts
@@ -3,12 +3,25 @@ import { Http } from '@angular/http';
 
 @Component({
 	selector: 'gallery',
+	host: { '(window:keydown)': 'hotkeys($event)' },
 	template: `
 	<div class="modal fade" id="selectedImageModal" >
 	  <div class="modal-dialog" role="document">
 		<div class="modal-content">
 		  <div class="modal-body">
-			 <img *ngIf="selectedImage" src="{{selectedImage.url}}" >
+			<div class="selectedImage" *ngIf="selectedImage">
+                                <img src="{{selectedImage.url}}" >
+<div class="arrow-back" (click)=navigate(false)>
+      &lt;
+   </div>
+   <div class="arrow-forward" (click)=navigate(true)>
+      &gt;
+   </div>
+			   <div class="caption">
+				  <p><strong>{{selectedImage.title}}</strong></p>
+				  <p>{{selectedImage.caption}}</p>
+			   </div>
+			</div>
 		  </div>
 		</div>
 	  </div>
@@ -22,7 +35,13 @@ import { Http } from '@angular/http';
 	   </li>
 	</ul>
   `,
-	styles: [`
+    styles: [`
+i {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+}
 	ul { padding:0; width:780px; margin:20px auto}
 	li { display:inline;}
 		.tn{ 
@@ -30,32 +49,60 @@ import { Http } from '@angular/http';
 	   box-shadow:#999 1px 1px 3px 1px; 
 	   cursor: pointer 
 		}
+img {
+    position: relative;
+	max-width: 100%;
+	max-height: 100%;
+}
 	.modal-content {
 		width: 670px !important;
+        height: 670px !important;
+        background: antiquewhite
 	}
+	.selectedImage{ 
+		width:640px;
+		position:relative }
+	.caption{
+		position:absolute;
+		width:100%;
+		bottom:0;
+		opacity:0.5;
+		background-color:black;
+		color:white;
+		padding:5px;
+		font-family:verdana;
+		font-size:12px;
+	}
+	p {
+	   -webkit-margin-before: 5px !important;
+	   -webkit-margin-after: 5px !important;
+	}
+.arrow-back, .arrow-forward{
+	position:absolute;
+	opacity:0.5;
+	top:50%;
+	color:whitesmoke;
+	font-weight:900;
+    font-size: xx-large;
+	cursor:pointer;
+}
+.arrow-left{
+	margin-left:5px;
+}
+.arrow-forward{
+    margin-right:5px;
+	right:0;
+}
   `]
 })
+
+    
 export class GalleryComponent {
 
 	@Input() datasource;
 	selectedImage;
 
 	constructor(private http: Http) {
-		//this.datasource = [
-		//	{ "url": "http://lorempixel.com/output/city-q-c-1920-1920-9.jpg" },
-		//	{ "url": "http://lorempixel.com/output/sports-q-c-1920-1920-2.jpg" },
-		//	{ "url": "http://lorempixel.com/output/animals-q-c-1920-1920-8.jpg" },
-		//	{ "url": "http://lorempixel.com/output/city-q-c-1920-1920-5.jpg" },
-		//	{ "url": "http://lorempixel.com/output/nature-q-c-1920-1920-7.jpg" },
-		//	{ "url": "http://lorempixel.com/output/abstract-q-c-1920-1920-8.jpg" },
-		//	{ "url": "http://lorempixel.com/output/nature-q-c-1920-1920-1.jpg" },
-		//	{ "url": "http://lorempixel.com/output/people-q-c-1920-1920-7.jpg" },
-		//	{ "url": "http://lorempixel.com/output/animals-q-c-1920-1920-1.jpg" },
-		//	{ "url": "http://lorempixel.com/output/technics-q-c-1920-1920-8.jpg" },
-		//	{ "url": "http://lorempixel.com/output/abstract-q-c-1920-1920-1.jpg" },
-		//	{ "url": "http://lorempixel.com/output/food-q-c-1920-1920-9.jpg" }
-		//];
-
 		this.http.get('/api/gallery/photo').subscribe(result => {
 			this.datasource = result.json();
 		});
@@ -63,5 +110,22 @@ export class GalleryComponent {
 
 	setSelectedImage(image) {
 		this.selectedImage = image;
+	}
+
+	navigate(forward) {
+		var index = this.datasource.indexOf(this.selectedImage) + (forward ? 1 : -1);
+		if (index >= 0 && index < this.datasource.length) {
+			this.selectedImage = this.datasource[index];
+		}
+	}
+
+	hotkeys(event) {
+		if (this.selectedImage) {
+			if (event.keyCode == 37) {
+				this.navigate(false);
+			} else if (event.keyCode == 39) {
+				this.navigate(true);
+			}
+		}
 	}
 }
